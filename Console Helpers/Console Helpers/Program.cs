ae4afda0253433c809d38d94f57331ca08ab5167
merzoh <mikejhoran@icloud.com>
2026-09-15 06:56:05 -0700

    Moved read masked keys to be apart of Console Helpers and created new folder for the class.

---
+++ Console Helpers/Console Helpers/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter sample text: ");
+        string maskedInput = ConsoleHelpers.ReadMaskedKey();
+        Console.WriteLine($"You entered: {maskedInput}");
+    }
+}
+
+public static class ConsoleHelpers
+{
+    public static string ReadMaskedKey(char maskChar = '*')
+    {
+        string input = string.Empty;
+        while (true)
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            if (keyInfo.Key == ConsoleKey.Enter)
+            {
+                Console.WriteLine();
+                return input;
+            }
+            else if (keyInfo.Key == ConsoleKey.Backspace)
+            {
+                if (input.Length > 0)
+                {
+                    Console.Write("\b \b");
+                    input = input[..^1];
+                }
+            }
+            else
+            {
+                input += keyInfo.KeyChar;
+                Console.Write(maskChar);
+            }
+        }
+    }
+}
