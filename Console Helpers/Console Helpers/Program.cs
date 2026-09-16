e222938ca301c25e003a0f177c2921105c5d86c0
merzoh <mikejhoran@icloud.com>
2026-09-16 11:28:52 -0700

    Started work on creating a method to draw an option menu and let the user select a choice.

--- Console Helpers/Console Helpers/Program.cs
+++ Console Helpers/Console Helpers/Program.cs
@@ -2,14 +2,51 @@
 {
     static void Main()
     {
-        Console.Write("Enter sample text: ");
-        string maskedInput = ConsoleHelpers.ReadMaskedKey();
-        Console.WriteLine($"You entered: {maskedInput}");
+        Console.CursorVisible = false;
+        ConsoleHelpers.DrawOptions();
+        Console.ReadKey(true);
     }
+}
+
+enum Options
+{
+    Message,
+    Exit,
 }
 
 public static class ConsoleHelpers
 {
+    public static void DrawOptions()
+    {
+        int windowWidth = Console.WindowWidth;
+        int windowHeight = Console.WindowHeight;
+        int optionsCharacterCount = string.Join("", Enum.GetNames(typeof(Options))).Length;
+        int padding = (windowWidth - optionsCharacterCount) / Enum.GetValues(typeof(Options)).Length;
+
+        DrawLine();
+        Console.SetCursorPosition(0, windowHeight - 1);
+        DrawLine();
+        Console.SetCursorPosition(0, windowHeight / 2);
+        foreach (Options option in Enum.GetValues(typeof(Options)))
+        {
+            DrawPadding();
+            Console.Write($"{option} ");
+        }
+        void DrawLine()
+        {
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("=");
+            }
+        }
+        void DrawPadding()
+        {
+            for (int i = 0; i < padding; i++)
+            {
+                Console.Write(" ");
+            }
+        }
+    }
     public static string ReadMaskedKey(char maskChar = '*')
     {
         string input = string.Empty;
