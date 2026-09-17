a0fe5377db825dbf7e6f54a1dc87aa9c6662eddc
merzoh <mikejhoran@icloud.com>
2026-09-17 11:50:44 -0700

    Created the constructor for OptionSelector which instantiates an array of MenuOptions populating each option with it's required values.

--- Console Helpers/Console Helpers/Program.cs
+++ Console Helpers/Console Helpers/Program.cs
@@ -13,37 +13,67 @@ enum Options
     Message,
     Exit,
 }
+public class MenuOptions
+{
+    public string OptionName { get; private set; }
+    public (int x, int y) StartPos { get; private set; }
+    public (int x, int y) EndPos { get; private set; }
+    public bool IsSelected { get; set; }
+    public MenuOptions(string optionName, (int x, int y) startPos, (int x, int y) endPos)
+    {
+        OptionName = optionName;
+        StartPos = startPos;
+        EndPos = endPos;
+        IsSelected = false;
+    }
+    public void SetName(string name)
+    {
+
+    }
+}
+public class OptionSelector
+{
+    //Fields and Properties
+    
+    private MenuOptions[] _options;
+    private int _selectedIndex = 0;
+    //Constructor that takes an enum type and creates a MenuOptions array based on the enum values
+    public OptionSelector(Enum enumType)
+    {
+        int padding = ((Console.WindowWidth - string.Join("", Enum.GetNames(enumType.GetType())).Length) / (Enum.GetValues(enumType.GetType()).Length + 1));
+        int optionY = (int)(Console.WindowHeight * (2f / 3f));
+        int currentX = padding;
+        Array values = Enum.GetValues(enumType.GetType());
+        _options = new MenuOptions[Enum.GetValues(enumType.GetType()).Length];
+        for (int i = 0; i < Enum.GetValues(enumType.GetType()).Length; i++)
+        {
+            string name = values.GetValue(i).ToString();
+            _options[i] = new MenuOptions(name, (currentX, optionY), (currentX + name.Length, optionY));
+            currentX += name.Length + padding;
+        }
+    }
+    //Methods
+
+}
 
 public static class ConsoleHelpers
 {
-    public static void DrawOptions()
+    public static void DrawOptions(MenuOptions[] options, int selectedIndex)
     {
         int windowWidth = Console.WindowWidth;
         int windowHeight = Console.WindowHeight;
-        int optionsCharacterCount = string.Join("", Enum.GetNames(typeof(Options))).Length;
-        int padding = (windowWidth - optionsCharacterCount) / Enum.GetValues(typeof(Options)).Length;
+        
 
         DrawLine();
         Console.SetCursorPosition(0, windowHeight - 1);
         DrawLine();
-        Console.SetCursorPosition(0, windowHeight / 2);
-        foreach (Options option in Enum.GetValues(typeof(Options)))
-        {
-            DrawPadding();
-            Console.Write($"{option} ");
-        }
+        
+        
         void DrawLine()
         {
             for (int i = 0; i < Console.WindowWidth; i++)
             {
                 Console.Write("=");
-            }
-        }
-        void DrawPadding()
-        {
-            for (int i = 0; i < padding; i++)
-            {
-                Console.Write(" ");
             }
         }
     }
